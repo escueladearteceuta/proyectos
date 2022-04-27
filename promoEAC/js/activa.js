423337c1906eb17c4b7864e1070a45c288f8bd74
escueladearteceuta <antonio.magraner@eaceuta.com>
2022-04-27 19:41:27 +0200

    Nuevo diseño

---
+++ promoEAC/js/activa.js
@@ -0,0 +1,14 @@
+
+
+const boton = document.querySelector(".enlace")
+
+boton.addEventListener("click", function(event){
+
+	const snatch_bot = document.querySelector("#sntch_button")
+
+	console.log(snatch_bot)
+
+	snatch_bot.click();
+
+})
+
